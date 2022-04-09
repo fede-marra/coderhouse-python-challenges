3431dee8288e5bd0f2fcd5cd040546761ce4dc16
Federico Marra <fedemarra@gmail.com>
2022-04-09 16:02:44 -0300

    Refactor function definition

--- odd-even/main.py
+++ odd-even/main.py
@@ -1,8 +1,10 @@
-def odd_even(numero):
-    if numero % 2 == 0:
+def is_even(numero):
+    return numero % 2 == 0
+
+numero = int(input("Ingrese un numero entero por favor o 0 para terminar la ejecucion: "))
+while numero != 0:
+    if is_even(numero):
         print(f"El numero {numero} es par")
     else:
         print(f"El numero {numero} es impar")
-
-numero = int(input("Ingrese un numero entero por favor: "))
-odd_even(numero)
+    numero = int(input("Ingrese un numero entero por favor o 0 para terminar la ejecucion: "))
